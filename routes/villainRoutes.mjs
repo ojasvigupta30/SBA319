a8431fa97bd0426f337c074c99691b7100c447f7
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-10-23 23:25:45 -0500

    villainRoutes added

--- routes/villainRoutes.mjs
+++ routes/villainRoutes.mjs
@@ -8,6 +8,10 @@ const router = express.Router();
 router.post(`/`, async(reqs, resp)=>{
 
     try {
+
+        const newVillain = new Villain(reqs.body);
+        await newVillain.save();
+        resp.json(newVillain);
         
     } catch (err) {
 
@@ -19,14 +23,60 @@ router.post(`/`, async(reqs, resp)=>{
 });
 
 //READ
-router.get(`/`, (reqs, resp)=>{
-    resp.send(`Villain Testing`);
-})
+router.get(`/`, async(reqs, resp)=>{
+
+    try {
+
+        const allVillains = await Villain.find({});
+        resp.json(allVillains);
+        
+        
+    } catch (err) {
+
+        console.error(err);
+        resp.status(500).json({msg:`Server Error`});
+        
+    }
+
+});
+
 
 //UPADTE
+router.put(`/:alias`, async(reqs, resp)=>{
+
+    try {
+
+        let updatedVillain = await Villain.findOneAndReplace({alias: reqs.params.alias}, reqs.body, {new:true});
+        resp.json(updatedVillain);
+
+        
+        
+    } catch (err) {
+
+        console.error(err);
+        resp.status(500).json({msg:`Server Error`});
+        
+    }
+
+});
 
 
 //DELETE
+router.delete(`/:alias`, async(reqs, resp)=>{
+
+    try {
+
+        await Villain.findOneAndDelete({alias: reqs.params.alias});
+        resp.json({msg: `Villain ${reqs.params.alias} deleted`});
+        
+    } catch (err) {
+
+        console.error(err);
+        resp.status(500).json({msg:`Server Error`});
+        
+    }
+
+});
 
 
 //Exports
