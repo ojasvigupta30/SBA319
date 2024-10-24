ad2b7fe9f886b3b1452a24a673b8b10b3ebc1f67
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-10-23 23:16:11 -0500

    heroRoutes added

--- routes/heroRoutes.mjs
+++ routes/heroRoutes.mjs
@@ -5,21 +5,77 @@ import Hero from '../models/heroSchema.mjs';
 const router = express.Router();
 
 //CREATE
+router.post(`/`, async (reqs,resp)=>{
 
+    try {
+
+        const newHero = new Hero(reqs.body);
+        await newHero.save();
+        resp.json(newHero);
+
+        
+    } catch (err) {
+
+        console.error(err);
+        resp.status(500).json({msg: `Server Error`});
+        
+    }
+
+});
 
 
 //READ
-router.get(`/`, (reqs, resp)=>{
-    resp.send(`Hero Testing`);
-})
+router.get(`/`, async (reqs,resp)=>{
+
+    try {
+
+        const allHeros = await Hero.find({});
+        resp.json(allHeros);
+        
+    } catch (err) {
+
+        console.error(err);
+        resp.status(500).json({msg: `Server Error`});
+        
+    }
+
+});
 
 
 //UPDATE
+router.put(`/:alias`, async (reqs,resp)=>{
 
+    try {
+
+        let updatedHero = await Hero.findOneAndReplace({ alias: reqs.params.alias }, reqs.body, { new: true });
+        resp.json(updatedHero);
+        
+    } catch (err) {
+
+        console.error(err);
+        resp.status(500).json({msg: `Server Error`});
+        
+    }
+
+});
 
 
 //DELETE
+router.delete(`/:alias`, async (reqs,resp)=>{
 
+    try {
+
+        await Hero.findOneAndDelete({ alias: reqs.params.alias });
+        resp.json({msg: `Hero ${reqs.params.alias} deleted`});
+        
+    } catch (err) {
+
+        console.error(err);
+        resp.status(500).json({msg: `Server Error`});
+        
+    }
+
+});
 
 
 //Exports
--- routes/villainRoutes.mjs
+++ routes/villainRoutes.mjs
@@ -5,7 +5,18 @@ import Villain from '../models/villainSchema.mjs';
 const router = express.Router();
 
 //CREATE
+router.post(`/`, async(reqs, resp)=>{
 
+    try {
+        
+    } catch (err) {
+
+        console.error(err);
+        resp.status(500).json({msg:`Server Error`});
+        
+    }
+
+});
 
 //READ
 router.get(`/`, (reqs, resp)=>{
