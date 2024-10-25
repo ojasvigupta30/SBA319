7d4beaeaef728d56c908951f49ebf12b1211747f
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-10-24 23:13:23 -0500

    seedRoute added

---
+++ routes/seedRoute.mjs
@@ -0,0 +1,29 @@
+import express from 'express';
+import Hero from '../models/heroSchema.mjs';
+import heroes from '../data/heroes.mjs';
+import Villain from '../models/villainSchema.mjs';
+import villains from '../data/villains.mjs';
+import Battle from '../models/battleSchema.mjs';
+import battles from '../data/battles.mjs';
+
+const router = express.Router();
+
+
+
+router.get(`/seed`, async (reqs, resp)=>{
+
+    //Optional step to erase the database first
+    await Hero.deleteMany({});
+    await Villain.deleteMany({});
+    await Battle.deleteMany({});
+
+    //create items in database
+await Hero.create(heroes);
+await Villain.create(villains);
+await Battle.create(battles);
+
+resp.send(`Seeding Database`);
+
+});
+
+export default router;
